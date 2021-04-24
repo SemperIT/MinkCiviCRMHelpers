32f6b5afdb1521aa4c8ed514841360166d3bcc8a
demeritcowboy <demeritcowboy@hotmail.com>
2021-04-24 16:16:56 -0400

    move createContact into shared

--- tests/src/Traits/Utils.php
+++ tests/src/Traits/Utils.php
@@ -70,12 +70,11 @@ trait Utils {
     // Check civi status messages
     // This would be a more robust way but it doesn't work here because once the
     // message is displayed it's removed from the array, so we're too late.
-    /*
-    $session_messages = array_filter(\CRM_Core_Session::singleton()->getStatus(), function($x) {
-      return ($x['type'] === 'error');
-    });
-    $this->assertEmpty($session_messages);
-     */
+    //
+    // $session_messages = array_filter(\CRM_Core_Session::singleton()->getStatus(), function($x) {
+    //   return ($x['type'] === 'error');
+    // });
+    // $this->assertEmpty($session_messages);
 
     // This does almost the same thing but using the UI. This might be a little
     // more comprehensive because some of these are generated purely from
@@ -92,6 +91,48 @@ trait Utils {
    */
   protected function getBrowserOutputDirectory(): string {
     return DRUPAL_ROOT . '/sites/simpletest/browser_output/';
+  }
+
+  /**
+   * Create a contact.
+   * The civi core test framework includes a function called
+   * individualCreate(). While it is technically possible to get access to it
+   * in this environment, it's mostly a fluke and there's no guarantee that
+   * will continue. This function is a replacement for it. Alternatively,
+   * call the api directly yourself.
+   *
+   * Example: createContact(0, ['middle_name' => 'Apple']) would create a
+   * contact named "Anthony Apple Anderson", whereas
+   * createContact(1, ['first_name' => 'Jane']) would create a contact
+   * "Jane Miller".
+   *
+   * @param int $index There are a couple stock contacts. You can pick a
+   *   different index to get some different names etc to use as the base.
+   * @param array $params Some more params to merge into the base.
+   * @return array
+   *   An api3 result array - the contact data.
+   */
+  protected function createContact(int $index = 0, array $params = []): array {
+    $stockContacts = [
+      'first_name' => ['Anthony', 'Joe', 'Terrence', 'Lucie', 'Albert', 'Bill', 'Kim'],
+      'last_name' => ['Anderson', 'Miller', 'Smith', 'Collins', 'Peterson', 'Johnson', 'Li'],
+    ];
+    $vars = array_merge([
+      'contact_type' => 'Individual',
+      'first_name' => $stockContacts['first_name'][$index],
+      'last_name' => $stockContacts['last_name'][$index],
+      'email' => strtolower("{$stockContacts['first_name'][$index]}.{$stockContacts['last_name'][$index]}@example.org"),
+      'phone' => preg_replace('/[^0-9]/', '', bin2hex("{$stockContacts['first_name'][$index]}{$stockContacts['last_name'][$index]}")),
+    ], $params);
+
+    // Phone doesn't work the same as email for create.
+    // If the input params didn't blank it out, convert to right format.
+    if (!empty($vars['phone'])) {
+      $vars['api.Phone.create'] = ['phone' => $vars['phone']];
+      unset($vars['phone']);
+    }
+
+    return civicrm_api3('Contact', 'create', $vars);
   }
 
 }
